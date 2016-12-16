216bcaf028efd29ab185eb44507dcca1c3739781
Prash Medirattaa <prash16@gmail.com>
2016-12-16 15:12:15 -0800

    changes

--- exercise1_op/sd.py
+++ exercise1_op/sd.py
@@ -1,6 +1,9 @@
 def standard_deviation(x):
-    n = len(x)
-    mean = sum(x) / n
-    ssq = sum((x_i-mean)**2 for x_i in x)
-    stdev = (ssq/n)**0.5
-    return(stdev)
+    if n=1:
+        print("there is no standard deviation of 1 number")
+    else:
+        n = len(x)
+        mean = sum(x) / n
+        ssq = sum((x_i-mean)**2 for x_i in x)
+        stdev = (ssq/n)**0.5
+        return(stdev)
